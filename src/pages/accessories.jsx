04f3713d4dba25ba63a8dbfe25273dc0bb755fee
mzar <152801523+jadden11@users.noreply.github.com>
2024-10-12 16:44:15 +0700

    Update accessories.jsx

--- src/pages/accessories.jsx
+++ src/pages/accessories.jsx
@@ -12,6 +12,7 @@ const products = [
 
 export default function Accessories() {
   return (
+    <>
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
@@ -35,5 +36,7 @@ export default function Accessories() {
         </div>
       </div>
     </div>
+    </>
+      
   );
 }
