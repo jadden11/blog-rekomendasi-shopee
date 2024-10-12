d8f1c12dd77f1487233dca674e003b8890af416e
mzar <152801523+jadden11@users.noreply.github.com>
2024-10-12 16:45:36 +0700

    Update accessories.jsx

--- src/pages/accessories.jsx
+++ src/pages/accessories.jsx
@@ -16,9 +16,9 @@ export default function Accessories() {
       <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-            Foods
+            Accessories
           </h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">Makanan.</p>
+          <p className="mt-6 text-base leading-7 text-gray-600">Accessories.</p>
         </div>
       </main>
 
